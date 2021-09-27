bee9b70c874b7fc2e01efd05e61ccbb3dd89d18e
natshabat <teximnat@gmail.com>
2021-09-27 17:05:22 +0300

    HW2_Part1 @elpiankova"

---
+++ HW2_part1.py
@@ -0,0 +1,3 @@
+x = input('Please,write some sentence for our exercise there:')
+print(x, x[2], x[-2], x[:5], x[:-2], x[0:-1:2], x[1:-1:2], x[::-1],
+      x[::-2], x[-2::-2], x[-1:1:-1], len(x), sep='\n')
