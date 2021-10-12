395ab8f4191ca6692f6aa92b04c232c8e8fa7301
natshabat <teximnat@gmail.com>
2021-10-12 15:28:32 +0300

    HW2_ex10 @elpiankova"

--- HW2_ex10.py
+++ HW2_ex10.py
@@ -1,20 +1,26 @@
 import string
+# Создайте строку, в которой написан, какой-то текст.
+# Посчитайте сколько слов в тексте (разбейте на слова методом строк split)
+# Удалите знаки препинания в списке слов (пройдитесь циклом все слова и удалите методом strip знаки препинания)
+# Выведите слова в алфавитном порядке (найдите метод списка, который сортирует)
+# Посчитать, сколько раз встречается каждое слово:
 
-a = 'We are not what we should be! We are not what we need to be. But at least we are not what we used to be'
+a = "We are not what we should be!\n We are not what we need to be.\n But at least we are not what we used to be"
+print(a)
 a = a.lower()
+a = a.lstrip()
 b = a.split()
-print(len(a.split()))
-print(b)
+print(len(b))
+
 
 for word in string.punctuation:
     if word in a:
-        c = a = a.replace(word, '')
-print(c)
-y = c.split()
-print(y)
+        a = a.replace(word, '')
+print(a)
+# y = (sorted(a.split()))
+# print(y)
 
-print(sorted(y))
+x = dict((i, (sorted(a.split())).count(i)) for i in (sorted(a.split())))
 
-x = dict((i, y.count(i)) for i in y)
-print(x)
-
+for key, value in x.items():
+    print(key, ':', value)
