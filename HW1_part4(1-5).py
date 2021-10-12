6c2119112f53f9dc489fc08f91b52b12f01b1e18
natshabat <teximnat@gmail.com>
2021-10-12 11:27:44 +0300

    HW1_part4(1-5) @elpiankova"

--- HW1_part4(1-5).py
+++ HW1_part4(1-5).py
@@ -1,20 +1,22 @@
 # Создать словарь х-тик продукта с ключами "title" (str), "price"(float), "ingredients" (list)
 # 1. Добавить еще одну пару ключ-значение "descriptions": какой-то текст
-pr = {"title": "potato","price": 20.5, "ingredients": (1,2,3)}
+pr = {"title": "potato", "price": 20.5, "ingredients": [1, 2, 3]}
 print(pr)
 pr["descriptions"] = 'red'
 print(pr)
 print( '-------------------------- next exercise 2 ')
 # 2. Увеличить цену на 100
-print(pr["price"]+100)
-print( '-------------------------- next exercise 3 ')
+pr["price"] = pr["price"]+100
+print(pr)
+print('-------------------------- next exercise 3 ')
 # 3. Добавить в список инградиентов еще один инградиент.
-pr["ingredients"] = pr["ingredients"],+2,+5
+pr["ingredients"].append(2)
+pr["ingredients"].append(5)
 print(pr)
 print( '-------------------------- next exercise 4 ')
 # 4. Вывести на экран кол-во инградиентов.
 print(len(pr["ingredients"]))
 print( '-------------------------- next exercise 5 ')
 # 5. Удалить из словаря значение с ключем 'descriptions'
-del pr['descriptions']
+pr.pop('descriptions')
 print(pr)
